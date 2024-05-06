c6fc6bb594083eb481fe65a3180af4eada01d95d
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2024-05-06 17:39:56 +0100

    more tutorial progress

--- Entities/UFO.gd
+++ Entities/UFO.gd
@@ -1,11 +1,19 @@
 extends RigidBody2D
 
-
+var movementSpeed: float = 1050.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#apply_impulse(Vector2(25,0))
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	if(Input.is_action_pressed("move_right")):
+		apply_force(Vector2(movementSpeed,0))
+	if(Input.is_action_pressed("move_left")):
+		apply_force(Vector2(-movementSpeed,0))
+	if(Input.is_action_pressed("move_up")):
+		apply_force(Vector2(0,-movementSpeed))
+	if(Input.is_action_pressed("move_down")):
+		apply_force(Vector2(0,movementSpeed))
